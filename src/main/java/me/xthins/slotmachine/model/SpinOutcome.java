c8fab37043132bbba36a9aa94ed496bab1f123d8
kevinzicker-byte <kevin.zicker@gmail.com>
2026-03-06 22:54:00 -0600

    Update SpinOutcome.java

--- src/main/java/me/xthins/slotmachine/model/SpinOutcome.java
+++ src/main/java/me/xthins/slotmachine/model/SpinOutcome.java
@@ -6,18 +6,42 @@ public class SpinOutcome {
     private final boolean nearMiss;
     private final double payout;
     private final ReelSymbol symbol;
+    private final boolean freeSpinsTriggered;
 
     public SpinOutcome(boolean won, boolean jackpot, boolean nearMiss, double payout, ReelSymbol symbol) {
+        this(won, jackpot, nearMiss, payout, symbol, false);
+    }
+
+    public SpinOutcome(boolean won, boolean jackpot, boolean nearMiss, double payout, ReelSymbol symbol, boolean freeSpinsTriggered) {
         this.won = won;
         this.jackpot = jackpot;
         this.nearMiss = nearMiss;
         this.payout = payout;
         this.symbol = symbol;
+        this.freeSpinsTriggered = freeSpinsTriggered;
     }
 
-    public boolean isWon() { return won; }
-    public boolean isJackpot() { return jackpot; }
-    public boolean isNearMiss() { return nearMiss; }
-    public double getPayout() { return payout; }
-    public ReelSymbol getSymbol() { return symbol; }
+    public boolean isWon() {
+        return won;
+    }
+
+    public boolean isJackpot() {
+        return jackpot;
+    }
+
+    public boolean isNearMiss() {
+        return nearMiss;
+    }
+
+    public double getPayout() {
+        return payout;
+    }
+
+    public ReelSymbol getSymbol() {
+        return symbol;
+    }
+
+    public boolean isFreeSpinsTriggered() {
+        return freeSpinsTriggered;
+    }
 }
